c870e8550f5e97606091bdf899086c32abce4600
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-14 22:22:44 +0530

    Initial Commit

---
+++ Ace Technosys-SwiftUI/Ace_Technosys_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ace_Technosys_SwiftUIApp.swift
+//  Ace Technosys-SwiftUI
+//
+//  Created by Nimish Mothghare on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Ace_Technosys_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ace Technosys-SwiftUITests/Ace_Technosys_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Ace_Technosys_SwiftUITests.swift
+//  Ace Technosys-SwiftUITests
+//
+//  Created by Nimish Mothghare on 14/04/25.
+//
+
+import Testing
+@testable import Ace_Technosys_SwiftUI
+
+struct Ace_Technosys_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
