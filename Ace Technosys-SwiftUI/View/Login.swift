c7403c72318cd9f16766f81da3cf19ee25c1d2d3
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-14 22:58:05 +0530

    Xcode update Changes

---
+++ Ace Technosys-SwiftUI/View/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Ace Technosys-SwiftUI
+//
+//  Created by Nimish Mothghare on 14/04/25.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
